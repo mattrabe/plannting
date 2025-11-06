8172994afae8ce32d542936d63e104d5a679a201
Matt Rabe <mrabe@completecodesolutions.com>
2025-11-06 07:25:55 -1000

    Create Fertilizer

---
+++ apps/api/src/endpoints/fertilizers/createFertilizer.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+
+import { Fertilizer } from '../../models'
+
+import { publicProcedure } from '../../procedures/publicProcedure'
+
+export const createFertilizer = publicProcedure
+  .input(z.object({
+    name: z.string(),
+    type: z.enum(['liquid', 'granules']),
+    isOrganic: z.boolean(),
+    notes: z.string().optional(),
+    nitrogen: z.number(),
+    phosphorus: z.number(),
+    potassium: z.number(),
+  }))
+  .mutation(async ({ input }) => {
+    const fertilizer = await Fertilizer.create(input)
+
+    return fertilizer.toObject()
+  })
