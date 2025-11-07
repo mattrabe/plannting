b2f96eb7e22baeae2cfe51f30c21869234f80a09
Matt Rabe <mrabe@completecodesolutions.com>
2025-11-06 21:06:21 -1000

    Chores list view (#14)

--- apps/api/src/routers/chores.ts
+++ apps/api/src/routers/chores.ts
@@ -2,11 +2,13 @@ import { router } from '../trpc'
 
 import { createChore } from '../endpoints/chores/createChore'
 import { deleteChore } from '../endpoints/chores/deleteChore'
+import { listChores } from '../endpoints/chores/listChores'
 import { updateChore } from '../endpoints/chores/updateChore'
 
 export const choresRouter = router({
   create: createChore,
   delete: deleteChore,
+  list: listChores,
   update: updateChore,
 })
 
